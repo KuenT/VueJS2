0619fed6078d54741581a78b982a652524c8dbae
Quentin TENGA <quentin.tenga@valeo.com>
2022-08-03 17:17:22 +0200

    exercices passage parent-enfant

---
+++ exercice/src/components/Arbre.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <li v-for="(arbre, index) in arbres " :key='index'>{{ name }}</li>
+  <feuille></feuille>
+</div>
+</template>
+
+<script>
+import Feuille from './Feuille.vue'
+export default {
+    components: {
+        feuille: Feuille,
+    },
+  
+  data() {
+    return {
+        arbres: [
+            {name: 'arbreA', height: '101cm'},
+            {name: 'arbreB', height: '120cm'}
+        ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ exercice/src/components/Feuille.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <Arbre></Arbre>
+  </div>
+</template>
+
+<script>
+import Arbre from './Arbre.vue'
+export default {
+    components: {
+        Arbre 
+    },
+    data() {
+        return {
+            feuilles: [
+                {name: 'feuilleA', size: '19cm', color: 'rouge'},
+                {name: 'feuilleB', size: '20cm', color: 'vert'},
+                {name: 'feuilleC', size: '21cm', color: 'jaune'},
+                {name: 'feuilleD', size: '22cm', color: 'bleu'}
+            ]
+        } 
+    },
+}
+</script>
+
+<style>
+
+</style>
